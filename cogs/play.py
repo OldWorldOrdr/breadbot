256b4b9b0b44ef8e52d1410ddd22c2e6d161c24e
baggette <58569691+Baggette@users.noreply.github.com>
2022-07-30 06:31:29 -0500

    fix

--- cogs/play.py
+++ cogs/play.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
 
-class Test1(commands.Cog):
+class Play(commands.Cog):
     def __init__(self, client):
         self.client = client
     @discord.slash_command(name ="play", description ="Play a song")
-    async def test1(self, ctx):
+    async def play(self, ctx):
         await ctx.respond("Play what? Fortnite?")
 def setup(client):
-    client.add_cog(Test1(client))
+    client.add_cog(Play(client))
