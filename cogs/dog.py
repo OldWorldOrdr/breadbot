db9373b87f0c2865158f7f272d66720c18326e70
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-07-30 08:05:42 -0400

    dog

--- cogs/dog.py
+++ cogs/dog.py
@@ -2,11 +2,11 @@ import discord
 import json
 from discord.ext import commands
 
-class Play(commands.Cog):
+class Dog(commands.Cog):
     def __init__(self, client):
         self.client = client
     @discord.slash_command(name ="dog", description ="Dog picture")
     async def play(self, ctx):
         await ctx.respond("Pretend this text is a dog")
 def setup(client):
-    client.add_cog(Play(client))
+    client.add_cog(Dog(client))
