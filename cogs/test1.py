f0d3b01cbb3814905af74414c64e9e3fb1d93ba7
baggette <58569691+Baggette@users.noreply.github.com>
2022-07-30 06:34:02 -0500

    fix

--- cogs/test1.py
+++ cogs/test1.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Test1(commands.Cog):
     def __init__(self, client):
         self.client = client
-    @discord.slash_command(name ="tes1", description ="test1 test cmd")
+    @discord.slash_command(name ="test1", description ="test1 test cmd")
     async def test1(self, ctx):
         await ctx.respond("test 1 worked")
 def setup(client):
